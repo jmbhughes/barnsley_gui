62327765d8862e7ec1986277f3bc69281ee1597a
Marcus Hughes <hughes.jmb@gmail.com>
2024-01-21 18:06:57 -0700

    hacked in file io

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,7 @@
+use std::sync::mpsc::{channel, Receiver, Sender};
+
 use barnsley::animation::AnimationSequence;
+use barnsley::config::{Config, ImageSettings, EvaluationSettings, self};
 use barnsley::ifs::IFS;
 use barnsley::image::Image;
 use barnsley::transform::{
@@ -7,8 +10,11 @@ use barnsley::transform::{
 use barnsley::util::Color;
 use egui::{self, Ui, Vec2, FontId, RichText};
 use egui_extras::install_image_loaders;
+use std::fs::{File, self};
+use std::future::Future;
 //use core::slice::SlicePattern;
-use std::io::Cursor;
+use std::sync::Arc;
+use std::io::{Cursor, Write, Read};
 use strum::IntoEnumIterator;
 
 use crate::transform::Visualize;
@@ -26,6 +32,7 @@ pub struct MyApp {
     pub(crate) transform_to_delete: usize,
     pub(crate) rerender: bool,
     counter: u8,
+    text_channel: (Sender<String>, Receiver<String>),
 }
 
 impl Default for MyApp {
@@ -55,6 +62,7 @@ impl Default for MyApp {
             transform_to_delete: 0,
             rerender: true,
             counter: 0,
+            text_channel: channel(),
         }
     }
 }
@@ -107,6 +115,29 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
         install_image_loaders(ctx);
 
+        if let Ok(text) = self.text_channel.1.try_recv() {
+            let config: Config = serde_json::from_str(&text).unwrap();
+
+            let mut ifs0: IFS = IFS::new();
+
+            for transform in config.transforms.into_iter() {
+                ifs0.add_transform(transform);
+            }
+            self.animation_sequence.ifs_vec[0] = ifs0;
+
+            let mut target_ifs = IFS::new();
+            target_ifs.add_transform(LinearTransform::new(0.07927406, 0.4419875, -0.64647937, 0.19174504,Color{r: 0.13267994, g: 0.49911928, b:0.9295654},0.93828845).into());
+            target_ifs.add_transform(InverseJuliaTransform::new(1.1700816, 2.9560707, Color{r: 0.9284186, g: 0.4638964, b:0.20791459}, 0.95615274).into());
+            target_ifs.add_transform(InverseJuliaTransform::new(1.0998807, 1.9877317, Color{r: 0.41831225, g: 0.5540522, b:0.46177816}, 1.0506994).into());
+
+            self.animation_sequence.ifs_vec[1] = target_ifs;
+            self.width = config.image_settings.width as usize;
+            self.height = config.image_settings.height as usize;
+            self.num_iterations = config.evaluation_settings.num_iterations as usize;
+            self.num_points = config.evaluation_settings.num_points as usize;
+            self.rerender = true;
+        }
+
         egui::SidePanel::left("controls")
             .exact_width(400.0)
             .show(ctx, |ui| {
@@ -120,9 +151,11 @@ impl eframe::App for MyApp {
                 ui.separator();
                 ui.heading("Create");
                 if ui.button("Randomize").clicked() {
-                    for ifs in self.animation_sequence.ifs_vec.iter_mut() {
-                        ifs.randomize();
-                    }
+                    // for ifs in self.animation_sequence.ifs_vec.iter_mut() {
+                    //     ifs.randomize();
+                    // }
+                    
+                    self.animation_sequence.ifs_vec.get_mut(0).unwrap().randomize();
 
                     self.rendered_image = self.animation_sequence.animate_single_step(
                         self.width,
@@ -132,6 +165,35 @@ impl eframe::App for MyApp {
                         1,
                     );
                 };
+
+                #[cfg(not(target_arch = "wasm32"))]
+                if ui.button("Open parameters").clicked() {
+                    let path = rfd::FileDialog::new().add_filter("json", &["json"]).pick_file().unwrap(); 
+                    
+                    let mut file = File::open(path).unwrap();
+                    let mut data = String::new();
+                    file.read_to_string(&mut data).unwrap();
+                    let config: Config = serde_json::from_str(&data).unwrap();
+
+                    let mut ifs0: IFS = IFS::new();
+
+                    for transform in config.transforms.into_iter() {
+                        ifs0.add_transform(transform);
+                    }
+                    self.animation_sequence.ifs_vec[0] = ifs0;
+
+                    let mut target_ifs = IFS::new();
+                    target_ifs.add_transform(LinearTransform::new(0.07927406, 0.4419875, -0.64647937, 0.19174504,Color{r: 0.13267994, g: 0.49911928, b:0.9295654},0.93828845).into());
+                    target_ifs.add_transform(InverseJuliaTransform::new(1.1700816, 2.9560707, Color{r: 0.9284186, g: 0.4638964, b:0.20791459}, 0.95615274).into());
+                    target_ifs.add_transform(InverseJuliaTransform::new(1.0998807, 1.9877317, Color{r: 0.41831225, g: 0.5540522, b:0.46177816}, 1.0506994).into());
+
+                    self.animation_sequence.ifs_vec[1] = target_ifs;
+                    self.width = config.image_settings.width as usize;
+                    self.height = config.image_settings.height as usize;
+                    self.num_iterations = config.evaluation_settings.num_iterations as usize;
+                    self.num_points = config.evaluation_settings.num_points as usize;
+                    self.rerender = true;
+                }
 
                 #[cfg(not(target_arch = "wasm32"))]
                 if ui.button("Save image").clicked() {
@@ -150,6 +212,80 @@ impl eframe::App for MyApp {
                         image::ColorType::Rgb8);
                 }
 
+                #[cfg(not(target_arch = "wasm32"))]
+                if ui.button("Save parameters").clicked() {
+                    let path = rfd::FileDialog::new().add_filter("json", &["json"]).save_file().unwrap(); 
+                    
+                    let config = Config {
+                        image_settings: ImageSettings{ 
+                            width: self.width as u32,
+                            height: self.height as u32,
+                            path: "empty.png".into()
+                        },
+                        evaluation_settings: EvaluationSettings{
+                            num_iterations: self.num_iterations as u32,
+                            num_points: self.num_points as u32
+                        },
+                        transforms: self.animation_sequence.ifs_vec.get(0).unwrap().transforms.clone()
+                    };
+
+                
+                    let _ = File::create(path.to_str().unwrap()).unwrap();
+                    fs::write(path.to_str().unwrap(), serde_json::to_string(&config).unwrap()).unwrap();
+                }
+
+                #[cfg(target_arch = "wasm32")]
+                if ui.button("Open parameters").clicked() {
+                    let sender = self.text_channel.0.clone();
+                    let task = rfd::AsyncFileDialog::new().pick_file();
+                    // Context is wrapped in an Arc so it's cheap to clone as per:
+                    // > Context is cheap to clone, and any clones refers to the same mutable data (Context uses refcounting internally).
+                    // Taken from https://docs.rs/egui/0.24.1/egui/struct.Context.html
+                    let ctx = ui.ctx().clone();
+                    execute(async move {
+                        let file = task.await;
+                        if let Some(file) = file {
+                            let text = file.read().await;
+                            let _ = sender.send(String::from_utf8_lossy(&text).to_string());
+                            ctx.request_repaint();
+                        }
+                    });
+
+
+                    // let future = async {
+                    //     let file = rfd::AsyncFileDialog::new().add_filter("json", &["json"]).pick_file().await;
+                    //     let data = file.unwrap().read().await;
+                    //     // let config: Config = serde_json::from_slice(&file.unwrap().read().await.as_slice()).unwrap();
+                    // };
+                    // let _ = async_std::task::block_on(future);
+                    // ui.close_menu();
+
+                    // let config = future;
+                    // let mut file = File::open(file).unwrap();
+                    // let mut data = String::new();
+                    // file.read_to_string(&mut data).unwrap();
+                    // let config: Config = serde_json::from_str(&data).unwrap();
+
+                    // let mut ifs0: IFS = IFS::new();
+
+                    // for transform in config.transforms.into_iter() {
+                    //     ifs0.add_transform(transform);
+                    // }
+                    // self.animation_sequence.ifs_vec[0] = ifs0;
+
+                    // let mut target_ifs = IFS::new();
+                    // target_ifs.add_transform(LinearTransform::new(0.07927406, 0.4419875, -0.64647937, 0.19174504,Color{r: 0.13267994, g: 0.49911928, b:0.9295654},0.93828845).into());
+                    // target_ifs.add_transform(InverseJuliaTransform::new(1.1700816, 2.9560707, Color{r: 0.9284186, g: 0.4638964, b:0.20791459}, 0.95615274).into());
+                    // target_ifs.add_transform(InverseJuliaTransform::new(1.0998807, 1.9877317, Color{r: 0.41831225, g: 0.5540522, b:0.46177816}, 1.0506994).into());
+
+                    // self.animation_sequence.ifs_vec[1] = target_ifs;
+                    // self.width = config.image_settings.width as usize;
+                    // self.height = config.image_settings.height as usize;
+                    // self.num_iterations = config.evaluation_settings.num_iterations as usize;
+                    // self.num_points = config.evaluation_settings.num_points as usize;
+                    // self.rerender = true;
+                }
+
                 #[cfg(target_arch = "wasm32")]
                 if ui.button("Save image").clicked() {
                     let mut bytes: Vec<u8> = Vec::new();
@@ -164,6 +300,30 @@ impl eframe::App for MyApp {
                     };
                     let data = async_std::task::block_on(future);
                     ui.close_menu();
+                }
+
+                #[cfg(target_arch = "wasm32")]
+                if ui.button("Save parameters").clicked() {                    
+                    let config = Config {
+                        image_settings: ImageSettings{ 
+                            width: self.width as u32,
+                            height: self.height as u32,
+                            path: "empty.png".into()
+                        },
+                        evaluation_settings: EvaluationSettings{
+                            num_iterations: self.num_iterations as u32,
+                            num_points: self.num_points as u32
+                        },
+                        transforms: self.animation_sequence.ifs_vec.get(0).unwrap().transforms.clone()
+                    };
+
+                    let future = async move {
+                        let file = rfd::AsyncFileDialog::new().add_filter("json", &["json"]).save_file().await;
+                        file.unwrap().write(&serde_json::to_vec(&config).unwrap().as_slice()).await
+                    };
+                    let data = async_std::task::block_on(future);
+                    ui.close_menu();
+            
                 }
 
                 ui.separator();
@@ -271,4 +431,9 @@ impl eframe::App for MyApp {
             );
         });
     }
+}
+
+#[cfg(target_arch = "wasm32")]
+fn execute<F: Future<Output = ()> + 'static>(f: F) {
+    wasm_bindgen_futures::spawn_local(f);
 }
